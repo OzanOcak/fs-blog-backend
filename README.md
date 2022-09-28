c43ef5ae67252bcfa6ae7244c792c23d119b8d9c
OzanOcak <ozanocak@gmail.com>
2022-09-28 13:17:07 -0400

    json middleware

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ touch src/server.js
 yarn add express
 node src/server.js
 ```
+
+## middleware
+
+```console
+curl -X POST -H "Content-Type: application/json" http://localhost:8000/hey -d '{"name":"ozan"}'
+```
--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 app.get("/hello", (req, res) => {
   res.send("hello");
+  res.status(200);
+});
+
+app.post("/hey", (req, res) => {
+  console.log(req.body);
+  res.send(`hey ${req.body.name}`);
   res.status(200);
 });
 app.listen(8000, () => {
